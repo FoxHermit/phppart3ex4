13603a841a6f69f6968a2cc8c369b2351abb5c96
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-29 18:50:03 +0200

    corrigé

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <?php
+// Création du tableau array $départements
+$départements = array(
+  '59' => 'Nord',
+  '62' => 'Pas-De-Calais',
+  '76' => 'Seine-Maritime',
+  '60' => 'Oise',
+  '02' => 'Aisne',
+  '80' => 'Somme',
+  '27' => 'Eure'); // Pas de virgule sur le dernier)
+// On met des quotes sur les chiffres des départements ici car on ne vas pas les calculer, c'est des "noms"
+     ?>
+  </body>
+</html>
